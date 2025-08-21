87b71e95b64db2f52a409c9ee7f049dd19eba48a
freakwho <freakwho123@gmail.com>
2025-08-21 14:56:15 +0700

    Ready

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-    })
-});
+const menu = document.querySelector("#menu");
+const nav = document.querySelector(".links");
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
